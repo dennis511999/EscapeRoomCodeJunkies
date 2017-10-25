31e5e4379c2ba2e24ec2785ecd1a54ec00c8f5c4
dennis511999 <dennis.engelen121084@student.leeuwenborgh.nl>
2017-10-25 14:29:50 +0200

    New

--- EscapeRoomCodeJunkies/Program.cs
+++ EscapeRoomCodeJunkies/Program.cs
@@ -3,13 +3,82 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+
 
 namespace EscapeRoomCodeJunkies
 {
     class Program
-    {
+     {
+        public static bool Escape { get; private set; }
+
+
         static void Main(string[] args)
         {
+
+            Console.WindowHeight = 20;
+            Console.WindowWidth = 128;
+            string strMenuText = System.IO.File.ReadAllText(@"C:\Users\Dennis\Desktop\Project codejunkies periode 1\EscapeRoomCodeJunkies\Menu.txt");
+            Console.WriteLine(strMenuText);
+            Console.SetCursorPosition(2, 17);
+            Console.ReadLine();
+
+            Console.Clear();
+
+            string strText = "Intro: \n" +
+            "It’s the year 1888. \n" +
+            "It's 2:00 am when you left the pub.\n" +
+            "You walk through a dark alley when suddenly you got hit from behind on your head.\n" +
+            "You slowly regained conciousness.\n" +
+            "Where am I? \n" +
+            "Old man: you have been captured by me. \n" +
+            "Find your way out of my sewers, if you can that is. \n" +
+            "I will give you a hint. \n" +
+            "Start by finding the key to open your cell door. \n";
+
+            for (int i = 0; i < strText.Length; i++)
+            {
+                Console.Write(strText[i]);
+                Thread.Sleep(0
+                    );
+            }
+
+            Console.ReadLine();
+
+            Console.Clear();
+            string strBasementText = System.IO.File.ReadAllText(@"C:\Users\Dennis\Desktop\Project codejunkies periode 1\EscapeRoomCodeJunkies\Basement.txt");
+            Console.WriteLine(strBasementText);
+            Console.ReadLine();
+
+
+            string command;
+            command = Console.ReadLine();
+
+            do
+            {
+                if (Escape == true)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Congratulations! You Made it out! ");
+                    Console.WriteLine("Press any key to exit ");
+                    Console.ReadLine();
+
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("You didn't make it out in time, want to try again? (Y/N) ");
+
+                }
+
+
+            }
+            while (command == "Y");
+
+
+
         }
     }
 }
