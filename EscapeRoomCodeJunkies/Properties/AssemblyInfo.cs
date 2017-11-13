77e88fddb963c3b44e354f7806ce5688b31497d5
dennis511999 <dennis.engelen121084@student.leeuwenborgh.nl>
2017-11-13 10:27:04 +0100

    Assemblyinfo

--- EscapeRoomCodeJunkies/Properties/AssemblyInfo.cs
+++ EscapeRoomCodeJunkies/Properties/AssemblyInfo.cs
@@ -1,17 +1,18 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EscapeRoomCodeJunkies")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("EscapeRoomCodeJunk")]
+[assembly: AssemblyDescription("EscapeRoom made by CodeJunk")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CodeJunk")]
 [assembly: AssemblyProduct("EscapeRoomCodeJunkies")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("™")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("")]
+
